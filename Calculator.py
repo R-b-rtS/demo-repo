b6ac7d51d39143f870c8683db39872853f7a320d
R-b-rtS <r.b.swart@hotmail.com>
2024-10-23 04:27:54 +0000

    Pending changes exported from your codespace

--- Calculator.py
+++ Calculator.py
@@ -3,15 +3,17 @@
 #This will be the person class where we enter all their information to use the various methods we will create
 
 class Person:
-    def __init__(self, loan: float, duration: int, interest_rate: float):
+    def __init__(self, loan: float, duration: int, interest_rate: float, repaymet_freq: string):
         """
         Loan: The total loan amount in dollars
         Duration: The length of the mortgage
         Interest rate: Current annual interest rate as a percentage
+        Repayment freq: How often you pay the mortgage: Weekly, Fortnightly, Monthly
         """
         self.loan = loan
         self.duration = duration
         self.interest_rate = interest_rate
+        self.repayment_freq
 
     def repayment_calculator(self):
         pass
