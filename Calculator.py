3c388f6245462d7b8e257f6381e6b7e91be178d4
R-b-rtS <r.b.swart@hotmail.com>
2024-10-23 05:38:50 +0000

    added repayment freq method and made more readable

--- Calculator.py
+++ Calculator.py
@@ -3,15 +3,23 @@
 #This will be the person class where we enter all their information to use the various methods we will create
 
 class Person:
-    def __init__(self, loan: float, duration: int, interest_rate: float):
+    def __init__(self, loan: float, duration: int, interest_rate: float, repayment_freq: str):
         """
         Loan: The total loan amount in dollars
         Duration: The length of the mortgage
         Interest rate: Current annual interest rate as a percentage
+        Repayment freq: How often you pay the mortgage: Weekly, Fortnightly, Monthly
         """
         self.loan = loan
         self.duration = duration
         self.interest_rate = interest_rate
+        self.repayment_freq = repaymet_freq
 
-    def repayment_calculator(self):
-        pass
+    def repayment_frequency(self):
+        frequencies = {
+            "Weekly": 52,
+            "Monthly": 12,
+            "Fortnightly": 26
+        }
+        return frequencies.get(self.repayment_freq, 12)
+        
