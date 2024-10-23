9b03831d88470829a92c397d88c33399e6b5ec1b
R-b-rtS <r.b.swart@hotmail.com>
2024-10-23 04:24:16 +0000

    Added comments and more to class

--- Calculator.py
+++ Calculator.py
@@ -3,8 +3,15 @@
 #This will be the person class where we enter all their information to use the various methods we will create
 
 class Person:
-    def __init__(self, loan, duration, interest_rate):
+    def __init__(self, loan: float, duration: int, interest_rate: float):
+        """
+        Loan: The total loan amount in dollars
+        Duration: The length of the mortgage
+        Interest rate: Current annual interest rate as a percentage
+        """
         self.loan = loan
         self.duration = duration
         self.interest_rate = interest_rate
-        
+
+    def repayment_calculator(self):
+        pass
