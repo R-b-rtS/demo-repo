2187a8623d47fcf988d7c1c570b962e0b5d61d42
R-b-rtS <r.b.swart@hotmail.com>
2024-10-23 08:56:46 +0000

    fixed some math errors, still has some

--- Calculator.py
+++ Calculator.py
@@ -14,17 +14,23 @@ class Person:
         self.duration = duration
         self.interest_rate = interest_rate
         self.repayment_freq = repayment_freq
-
+    def __repr__(self):
+        return f"Hello your loan amount is " + str(self.loan) + ". The duration of your loan is"
     def repayment_frequency(self):
         frequencies = {
             "Weekly": 52,
             "Monthly": 12,
             "Fortnightly": 26
         }
-        return frequencies.get(self.repayment_freq, 12)
+        return frequencies.get(self.repayment_freq, 12) * self.duration
     
     def monthly_interest(self):
-        return self.interest_rate / 12
+        return self.interest_rate / 100 / 12
     
     def repayment_calc(self):
-        repayments = (self.loan * self.monthly_interest() * (1 + self.monthly_interest)** self.repayment_frequency) / ((1 + self.monthly_interest) ** self.repayment_frequency - 1)
+        repayments = (self.loan * self.monthly_interest() * (1 + self.monthly_interest())** self.repayment_frequency()) / ((1 + self.monthly_interest()) ** self.repayment_frequency() - 1)
+        return repayments
+
+person1 = Person(200000, 30, 6.24, "Monthly")
+print(person1)
+print(person1.repayment_calc())
